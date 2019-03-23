1e6b2c89ed23650a7bc90f5e81f983d56a892610
Jacob Oakman <jacoboakman@gmail.com>
2019-03-23 12:39:38 -0400

    Continued work on routing

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,8 +2,8 @@
   <b-navbar toggleable="md" type="dark" variant="info">
     <b-navbar-brand :to="{ name: 'Home' }">Account-Owner Home</b-navbar-brand>
     <b-navbar-nav>
-      <b-nav-item href="#">Owner Actions</b-nav-item>
-      <b-nav-item href="#">Owner Actions</b-nav-item>
+      <b-nav-item href="/">Home</b-nav-item>
+      <b-nav-item href="/about">About</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import router from './router'
 
 Vue.use(BootstrapVue)
 
@@ -11,7 +11,5 @@ Vue.config.productionTip = false
 
 new Vue({
 	router,
-	components: { App },
-	template: '<App/>',
 	render: h => h(App),
 }).$mount('#app')
