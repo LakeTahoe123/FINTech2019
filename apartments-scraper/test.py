09c2467e323136ece1f9456be9ed9e87168e8a26
alexschiffman <schiffmanalex@gmail.com>
2019-03-23 18:55:25 -0400

    added json output

--- apartments-scraper/test.py
+++ apartments-scraper/test.py
@@ -9,6 +9,11 @@ print(address)
 print(data[5][4]) #rent
 print(data[5][8]) #distance
 print(data[5][9]) #time
+print(data[5][21]) #picture url
+
+s = data[5][21]
+print(s[s.find('(')+1 : s.find(')')])
+
 '''
 
 for i in data[5]:
