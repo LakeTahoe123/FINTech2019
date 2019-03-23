e86b5dede118b9953068eb08d2664b57d50ccf77
Jacob Oakman <jacoboakman@gmail.com>
2019-03-23 13:12:41 -0400

    Continued messing with bootstrap-vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,28 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link>|
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view/>
-	</div>
+	<b-container fluid id="app">
+		<b-row>
+			<navbar/>
+		</b-row>
+		<b-container>
+			<b-row>
+				<b-col>
+					<router-view/>
+				</b-col>
+			</b-row>
+		</b-container>
+	</b-container>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+	name: "App",
+	components: {
+		Navbar
+	}
+};
+</script>
 
 <style>
 #app {
@@ -15,6 +31,5 @@
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
 }
 </style>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="info">
-    <b-navbar-brand :to="{ name: 'Home' }">Account-Owner Home</b-navbar-brand>
-    <b-navbar-nav>
-      <b-nav-item href="/">Home</b-nav-item>
-      <b-nav-item href="/about">About</b-nav-item>
-    </b-navbar-nav>
-  </b-navbar>
+	<b-navbar fluid toggleable="md" type="dark" variant="info">
+		<b-navbar-brand :to="{ name: 'Home' }">Account-Owner Home</b-navbar-brand>
+		<b-navbar-nav>
+			<b-nav-item to="/">Home</b-nav-item>
+			<b-nav-item to="/about">About</b-nav-item>
+		</b-navbar-nav>
+	</b-navbar>
 </template>
 <script>
 export default {
-  name: "Navbar"
+	name: "Navbar"
 };
 </script>
