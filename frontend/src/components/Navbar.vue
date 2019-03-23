b2181b4835df885951a6295b91f78bb16718e1e9
Jacob Oakman <jacoboakman@gmail.com>
2019-03-23 13:57:04 -0400

    Added "Sign in to save" to nav

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
-	<b-navbar fill toggleable="md" type="dark" variant="info">
+	<b-navbar fill toggleable="md" type="dark" variant="primary">
 		<b-navbar-brand :to="{ name: 'Home' }">Account-Owner Home</b-navbar-brand>
+
 		<b-navbar-nav>
 			<b-nav-item to="/">Home</b-nav-item>
 			<b-nav-item to="/about">About</b-nav-item>
+		</b-navbar-nav>
+
+		<b-navbar-nav class="ml-auto">
+			<b-nav-text right>Sign in to Save</b-nav-text>
 		</b-navbar-nav>
 	</b-navbar>
 </template>
